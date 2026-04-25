dfe210aa16b2f913f27daacded43378d05ee7b26
zadokmbiti <zadokmbiti@gmail.com>
2026-04-26 02:42:53 +0300

    feat: add Procfile and gunicorn for deployment

--- laundry/settings.py
+++ laundry/settings.py
@@ -42,6 +42,7 @@ PWA_APP_DEBUG_MODE = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -56,6 +57,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 TEMPLATES = [
