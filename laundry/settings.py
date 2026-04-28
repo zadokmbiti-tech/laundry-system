966be5c0e6a5b79ec3be2956730f84b2f23a959d
zadokmbiti <zadokmbiti@gmail.com>
2026-04-28 09:58:55 +0300

    fix: connect to postgres on railway

--- laundry/settings.py
+++ laundry/settings.py
@@ -84,12 +84,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'laundry.wsgi.application'
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 # ── M-Pesa ────────────────────────────────────────────────────────────────────
