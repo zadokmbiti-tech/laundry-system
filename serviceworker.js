0bb796001a84b91d83634721109315af8c21182c
zadokmbiti <zadokmbiti@gmail.com>
2026-04-29 00:55:40 +0300

    changes to sms and home page

--- serviceworker.js
+++ serviceworker.js
@@ -1,10 +1,18 @@
-self.addEventListener('install',event=> {
-  console.log('Service worker installed');
+const CACHE_NAME = 'freshwash-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/static/images/hero-bg.jpg',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
 });
 
-self.addEventListener('fetch',event =>{
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(respond => respond|| fetch(event.request))
+      .then(respond => respond || fetch(event.request))
   );
 });
